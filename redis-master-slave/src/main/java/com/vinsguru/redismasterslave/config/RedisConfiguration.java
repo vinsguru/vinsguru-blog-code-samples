50002235a15614a99923fa5d01a967ce0f10e703
vins <vino.infy@gmail.com>
2019-11-18 19:49:58 -0600

    redis/spring boot

--- redis-master-slave/src/main/java/com/vinsguru/redismasterslave/config/RedisConfiguration.java
+++ redis-master-slave/src/main/java/com/vinsguru/redismasterslave/config/RedisConfiguration.java
@@ -1,30 +1,69 @@
 package com.vinsguru.redismasterslave.config;
 
 
-import io.lettuce.core.ClientOptions;
 import io.lettuce.core.ReadFrom;
-import io.lettuce.core.masterreplica.MasterReplica;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.RedisStaticMasterReplicaConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
-import static io.lettuce.core.ReadFrom.SLAVE_PREFERRED;
+import java.util.List;
 
 @Configuration
+@ConfigurationProperties(prefix = "redis")
 public class RedisConfiguration {
+
+    private RedisInstance master;
+    private List<RedisInstance> slaves;
+
+    RedisInstance getMaster() {
+        return master;
+    }
+
+    void setMaster(RedisInstance master) {
+        this.master = master;
+    }
+
+    List<RedisInstance> getSlaves() {
+        return slaves;
+    }
+
+    void setSlaves(List<RedisInstance> slaves) {
+        this.slaves = slaves;
+    }
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
         LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
                 .readFrom(ReadFrom.REPLICA_PREFERRED)
                 .build();
-        RedisStaticMasterReplicaConfiguration staticMasterReplicaConfiguration = new RedisStaticMasterReplicaConfiguration("localhost");
-        staticMasterReplicaConfiguration.node("localhost", 16379)
-                .node("localhost", 26379);
+        RedisStaticMasterReplicaConfiguration staticMasterReplicaConfiguration = new RedisStaticMasterReplicaConfiguration(this.getMaster().getHost(), this.getMaster().getPort());
+        this.getSlaves().forEach(slave -> staticMasterReplicaConfiguration.addNode(slave.getHost(), slave.getPort()));
         return new LettuceConnectionFactory(staticMasterReplicaConfiguration, clientConfig);
+    }
+
+    private static class RedisInstance {
+
+        private String host;
+        private int port;
+
+        String getHost() {
+            return host;
+        }
+
+        void setHost(String host) {
+            this.host = host;
+        }
+
+        int getPort() {
+            return port;
+        }
+
+        void setPort(int port) {
+            this.port = port;
+        }
     }
 
 }
--- redis-master-slave/src/main/java/com/vinsguru/redismasterslave/controller/RedisController.java
+++ redis-master-slave/src/main/java/com/vinsguru/redismasterslave/controller/RedisController.java
@@ -1,23 +1,29 @@
 package com.vinsguru.redismasterslave.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-import java.net.URL;
+import java.util.Set;
 
 @RestController
 public class RedisController {
+
+    private static final String KEY = "VINSGURU";
 
     @Autowired
     private StringRedisTemplate template;
 
     @GetMapping("/{name}")
-    public void addLink(@PathVariable String name) {
-        template.opsForSet().add("name", name);
+    public void addToSet(@PathVariable String name) {
+        this.template.opsForSet().add(KEY, name);
     }
+
+    @GetMapping("/get")
+    public Set<String> getKeyValues() {
+        return this.template.opsForSet().members(KEY);
+    }
+
 }
