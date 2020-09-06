9bce35cb9f9ddb24cbb4abfa77656e7fe62e2290
vins <vino.infy@gmail.com>
2020-09-05 23:01:17 -0500

    added missing main class

--- kafka-stream/src/main/java/com/vinsguru/kafkastream/KafkaStreamApplication.java
+++ kafka-stream/src/main/java/com/vinsguru/kafkastream/KafkaStreamApplication.java
@@ -1,4 +1,15 @@
 package com.vinsguru.kafkastream;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
 public class KafkaStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamApplication.class, args);
+    }
+
 }
