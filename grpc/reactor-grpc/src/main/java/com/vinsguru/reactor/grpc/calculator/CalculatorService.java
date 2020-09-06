0470a33126930b43eac5502923c326aeda36a1a5
vins <vino.infy@gmail.com>
2020-09-05 22:32:07 -0500

    updated

--- grpc/reactor-grpc/src/main/java/com/vinsguru/reactor/grpc/calculator/CalculatorService.java
+++ grpc/reactor-grpc/src/main/java/com/vinsguru/reactor/grpc/calculator/CalculatorService.java
@@ -6,6 +6,7 @@ import com.vinsguru.calculator.ReactorCalculatorServiceGrpc;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class CalculatorService extends ReactorCalculatorServiceGrpc.CalculatorServiceImplBase {
@@ -21,36 +22,29 @@ public class CalculatorService extends ReactorCalculatorServiceGrpc.CalculatorSe
     @Override
     public Flux<Output> getAllDoubles(Mono<Input> request) {
         return request
-                    .map(Input::getNumber)
-                    .flatMapMany(i -> Flux.range(1, i))
-                    .map(i -> i * 2)
-                    .map(i -> Output.newBuilder().setResult(i).build());
+                .map(Input::getNumber)
+                .flatMapMany(i -> Flux.range(1, i))
+                .map(i -> i * 2)
+                .map(i -> Output.newBuilder().setResult(i).build());
     }
 
     @Override
     public Mono<Output> sumAll(Flux<Input> request) {
         return request
-                    .map(Input::getNumber)
-                    .reduce(Integer::sum)
-                    .map(i -> Output.newBuilder().setResult(i).build());
+                .map(Input::getNumber)
+                .reduce(Integer::sum)
+                .map(i -> Output.newBuilder().setResult(i).build());
     }
 
     @Override
     public Flux<Output> playUpTo100(Flux<Input> request) {
+        AtomicBoolean atomicBoolean = new AtomicBoolean(true);
         AtomicInteger sum = new AtomicInteger(0);
         return request
-                    .map(Input::getNumber)
-                    .map(i -> {
-                        sum.set(sum.get() + i);
-                        System.out.println("Sum so far :: "+ sum.get());
-                        return sum.get();
-                    })
-                    .map(i -> Output.newBuilder().setResult(i).build())
-                    .handle((output, outputSynchronousSink) -> {
-                        outputSynchronousSink.next(output);
-                        if(output.getResult() > 100)
-                            outputSynchronousSink.complete();
-                    });
+                .map(Input::getNumber)
+                .map(sum::addAndGet)
+                .takeWhile(i -> i < 100 ? atomicBoolean.get() : atomicBoolean.getAndSet(false))
+                .map(i -> Output.newBuilder().setResult(i).build());
     }
 
     private long factorial(int number){
