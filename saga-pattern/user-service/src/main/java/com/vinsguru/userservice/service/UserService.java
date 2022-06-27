8d136bf81b24d9cffd79af5672404ae9791f857f
vinsguru <vino.infy@gmail.com>
2022-06-27 08:12:57 -0600

    re-organized folder structure

--- saga-pattern/user-service/src/main/java/com/vinsguru/userservice/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.vinsguru.userservice.service;
-
-import com.vinsguru.common.dto.TripRequestDto;
-import com.vinsguru.common.dto.TripResponseDto;
-
-public interface UserService {
-    void createTrip(TripRequestDto tripRequestDto);
-    void updateTrip(TripResponseDto tripResponseDto);
-}
