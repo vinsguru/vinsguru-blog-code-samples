59694b2b5b747aa187e01d1126bdcd443a47e811
vinsguru <vino.infy@gmail.com>
2023-11-27 20:04:27 -0700

    updated for latest spring boot 3.2.0 + Java 21

--- resilient-microservice-pattern/timeout-pattern/product-service/src/main/java/com/vinsguru/product/config/AppConfig.java
+++ resilient-microservice-pattern/timeout-pattern/product-service/src/main/java/com/vinsguru/product/config/AppConfig.java
@@ -5,14 +5,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Configuration
-public class RestClientConfig {
+public class AppConfig {
 
     @Bean
     public RestClient restClient(@Value("${rating.service.endpoint}") String baseUrl) {
         return RestClient.builder()
                          .baseUrl(baseUrl)
                          .build();
+    }
+
+    @Bean
+    public ExecutorService executorService(){
+        return Executors.newVirtualThreadPerTaskExecutor();
     }
 
 }
