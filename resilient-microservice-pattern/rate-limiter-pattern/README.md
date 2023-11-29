fd206c53e8767fc969d999e4df6e931e80053fe2
vinsguru <vino.infy@gmail.com>
2023-11-28 20:32:15 -0700

    updated for latest spring boot 3.2.0 + Java 21

---
+++ resilient-microservice-pattern/rate-limiter-pattern/README.md
@@ -0,0 +1,8 @@
+# Rate Limiter Pattern
+
+## How To Run
+- Navigate to this project directory
+- Run `make run`
+- Access `http://localhost:8080/square/10`
+
+Check my blog [here](https://www.vinsguru.com/rate-limiter-pattern/) for more details on Rate Limiter Pattern.
--- resilient-microservice-pattern/rate-limiter-pattern/src/main/java/com/vinsguru/ratelimiter/controller/ComputeController.java
+++ resilient-microservice-pattern/rate-limiter-pattern/src/main/java/com/vinsguru/ratelimiter/controller/ComputeController.java
@@ -4,12 +4,16 @@ import com.vinsguru.ratelimiter.dto.ComputeResponse;
 import com.vinsguru.ratelimiter.dto.ResponseType;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.apache.logging.log4j.util.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ComputeController {
+
+    private static final Logger log = LoggerFactory.getLogger(ComputeController.class);
 
     @GetMapping("/double/{input}")
     public ComputeResponse doubleValue(@PathVariable int input){
@@ -23,6 +27,7 @@ public class ComputeController {
     }
 
     public ComputeResponse squareErrorResponse(int input, Throwable throwable){
+        log.warn("default response: {}", throwable.getMessage());
         return ComputeResponse.of(input, -1, ResponseType.FAILURE, throwable.getMessage());
     }
 
